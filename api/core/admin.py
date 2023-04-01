9eb88eb6197c775b651a842d0993f24865f21a2b
dmakger <dmakger@yandex.ru>
2023-04-01 22:58:24 +0300

    Добавил таблицы транзакций

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Nationality, Gender, Profile, TypeTransaction, PaymentMethodTransaction, CategoryTransaction, \
+    Transactions
+
+
+class NationalityAdmin(admin.ModelAdmin):
+    pass
+
+
+class GenderAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class TypeTransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+class PaymentMethodTransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryTransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+class TransactionsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Nationality, NationalityAdmin)
+admin.site.register(Gender, GenderAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(TypeTransaction, TypeTransactionAdmin)
+admin.site.register(PaymentMethodTransaction, PaymentMethodTransactionAdmin)
+admin.site.register(CategoryTransaction, CategoryTransactionAdmin)
+admin.site.register(Transactions, TransactionsAdmin)
--- api/core/models.py
+++ api/core/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import datetime as dt
 
 
 # ======{ Профиль }======
@@ -28,7 +29,7 @@ class Gender(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(verbose_name="Пользователь", to=User, on_delete=models.CASCADE)
+    # user = models.OneToOneField(verbose_name="Пользователь", to=User, on_delete=models.CASCADE)
     nationality = models.ForeignKey(to=Nationality, on_delete=models.SET_NULL, null=True, verbose_name="Гражданство")
     gender = models.ForeignKey(to=Gender, on_delete=models.SET_NULL, null=True, verbose_name="Гендер")
     birth_year = models.IntegerField()
@@ -47,3 +48,57 @@ def create_user(sender, **kwargs):
         user = kwargs['instance']
         Profile.objects.create(user=user).save()
 
+
+# ======{ Транзакции }======
+class TypeTransaction(models.Model):
+    title = models.CharField("Название", max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = "Тип транзакции"
+        verbose_name_plural = "Типы транзакции"
+
+    def __str__(self):
+        return self.title
+
+
+class PaymentMethodTransaction(models.Model):
+    title = models.CharField("Название", max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = "Способ платежа"
+        verbose_name_plural = "Способы платежа"
+
+    def __str__(self):
+        return self.title
+
+
+class CategoryTransaction(models.Model):
+    title = models.CharField("Название", max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = "Категория платежа"
+        verbose_name_plural = "Категории платежа"
+
+    def __str__(self):
+        return self.title
+
+
+class Transactions(models.Model):
+    time = models.DateTimeField("Время", default=dt.now)
+    profile = models.ForeignKey(to=Profile, on_delete=models.CASCADE, verbose_name="Пользователь")
+    type = models.ForeignKey(to=TypeTransaction, on_delete=models.CASCADE, verbose_name="Тип транзакции")
+    payment_method = models.ForeignKey(to=PaymentMethodTransaction, on_delete=models.CASCADE, verbose_name="Способ платежа")
+    category = models.ForeignKey(to=CategoryTransaction, on_delete=models.CASCADE, verbose_name="Категория платежа")
+    amount = models.FloatField("Сумма платежа", help_text="В рублях")
+    client_fee = models.FloatField("Комиссия клиента", default=0)
+    payout_fee = models.FloatField("Комиссия за выплату", default=0)
+    interchange_fee = models.FloatField("Плата за обмен", default=0)
+    pay_system_fee = models.FloatField("Системная комиссия", default=0)
+    paying_fee = models.FloatField("Пошлина", default=0)
+
+    class Meta:
+        verbose_name = "Транзакция"
+        verbose_name_plural = "Транзакции"
+
+    def __str__(self):
+        return self.title
